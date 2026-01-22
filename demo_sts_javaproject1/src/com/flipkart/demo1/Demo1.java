640f60662cce909669ad274332f469707417ff40
gnanachandrik <basagnanachandrika08@gmail.com>
2026-01-22 17:05:25 +0530

    my third commit

---
+++ demo_sts_javaproject1/src/com/flipkart/demo1/Demo1.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.flipkart.demo1;
+
+/**
+ * 
+ */
+public class Demo1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Third project of sts");
+	}
+
+}
