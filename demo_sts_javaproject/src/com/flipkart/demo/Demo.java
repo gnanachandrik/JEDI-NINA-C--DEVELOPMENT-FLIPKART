76e263d38616210f8064bcc288859802660a6243
gnanachandrik <basagnanachandrika08@gmail.com>
2026-01-22 19:32:59 +0530

    Remove unwanted file

--- demo_sts_javaproject/src/com/flipkart/demo/Demo.java
+++ demo_sts_javaproject/src/com/flipkart/demo/Demo.java
@@ -1,19 +1,19 @@
 /**
  * 
  */
-package com.flipkart.demo1;
+package com.flipkart.demo;
 
 /**
  * 
  */
-public class Demo1 {
+public class Demo {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("Third project of sts");
+         System.out.println("Second project by sts");
 	}
 
 }
